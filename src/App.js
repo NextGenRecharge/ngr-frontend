adeb41d72995f6e5f0dfb4033f6fdee563079ec6
nextgensolutionshiv <nextgensolutions@gmail.com>
2024-10-21 03:25:26 +0530

    Routing updated

--- src/App.js
+++ src/App.js
@@ -1,24 +1,9 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
-import SignUp from './components/SignUp/SignUp';
-import Login from './components/Landing/Landing';
-import ForgotPassword from './components/ForgotPassword';
-import Dashboard from './components/Dashboard/Home';
-import MPin from './pages/MPin/MPin';
-function App() {
-  const [user, setUser] = useState(null);
+import Routing from './routes/Routing';
 
+function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Navigate to="/login" />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/login" element={<Login setUser={setUser} />} />
-        <Route path="/forgot-password" element={<ForgotPassword />} />
-        <Route path="/dashboard" element={user ? <Dashboard user={user} /> : <Navigate to="/login" />} />
-        <Route path='/mpin' element={<MPin />} />
-      </Routes>
-    </Router>
+    <Routing />
   );
 }
 
